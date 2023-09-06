f23e0656247594b648d03276fdbc3accfec8727f
hikotq <hikotq@gmail.com>
2023-09-06 21:58:34 +0900

    step7

--- ip.h
+++ ip.h
@@ -4,6 +4,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "net.h"
+
 #define IP_VERSION_IPV4 4
 
 #define IP_HDR_SIZE_MIN 20
@@ -17,8 +19,24 @@
 
 typedef uint32_t ip_addr_t;
 
+struct ip_iface {
+    struct net_iface iface;
+    struct ip_iface *next;
+    ip_addr_t unicast;
+    ip_addr_t netmask;
+    ip_addr_t broadcast;
+};
+
 extern const ip_addr_t IP_ADDR_ANY;
 extern const ip_addr_t IP_ADDR_BROADCAST;
+
+extern struct ip_iface *
+ip_iface_alloc(const char *addr, const char *netmask);
+extern int
+ip_iface_register(struct net_device *dev, struct ip_iface *iface);
+extern struct ip_iface *
+ip_iface_select(ip_addr_t addr);
+
 
 extern int
 ip_addr_pton(const char *p, ip_addr_t *n);
